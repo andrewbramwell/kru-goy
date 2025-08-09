063574baf8920faa252292edb28fe0c02a92d0dc
andrewbramwell <andy@voucherstore.io>
2025-08-09 11:15:56 +0100

    update

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,24 +1,25 @@
 module.exports = {
-    "name": "Splendid Sites Web Designs",
-    "email": "help@splendidsites.com",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "openingHours": "Mon-Sun: 9am-9pm",
-    "onlineBooking": "https://www.facebook.com/",
+    "name": "Kru Goy Uk",
+    "email": "",
+    "phoneForTel": "+447415998445",
+    "phoneFormatted": "07415 998445",
+    "openingHours": "Mon-Sun: 10am-10pm",
+    "onlineBooking": "",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "lineOne": "48 Faulkner Street",
+        "lineTwo": "China Town",
+        "city": "Manchester",
+        "state": "",
+        "zip": "M1 4FH ",
+        "country": "UK",
+        "mapLink": ""
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "facebook": "https://www.facebook.com/kruGoySalisa",
+        "instagram": "",
         "googleReview": "",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://krugoyuk.co.uk"
 };
+
